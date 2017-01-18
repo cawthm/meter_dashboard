4e0395ef1ec9094fddad5bab34464189cef2bfce
cawthm <cawthm@mac.com>
2017-01-18 11:22:23 -0600

    consolidated library dependencies to helper_funcs; refactored everything to UTC

--- arcola_meter.R
+++ arcola_meter.R
@@ -1,13 +1,6 @@
 ## arcola meter
 # turning this into more DRY set of code to talk to ET meters
 
-library("httr")
-library("xml2")
-library("dplyr")
-library("readr")
-library("StreamMetabolism")
-library("lubridate")
-
 source("helper_funcs.R")
 
 arcola_location <- list(address = "52 Paramus Rd", 
@@ -15,7 +8,8 @@ arcola_location <- list(address = "52 Paramus Rd",
                         state = "NJ", 
                         tz = "US/Eastern", # use OlsonNames() to see list of valis tz's
                         lat = 40.923459, 
-                        lng = -74.086748) 
+                        lng = -74.086748,
+                        meter_type = "em") 
 
 
 arcola_base_url <- "http://arcolacovenant.synology.me:2856"
@@ -25,14 +19,15 @@ url_b <- paste0(arcola_base_url, "/rtdata.htm")
 
 
 while(year(Sys.Date()) <= 2017) {
-  daylight_span <- sunrise.set(arcola_location$lat, arcola_location$lng, date = ymd("2016-01-01", tz = arcola_location$tz))
+  daylight_span <- sunrise.set(arcola_location$lat, arcola_location$lng, date = ymd("2016-01-01", tz = "UTC")) # in UTC time
+  right_now_UTC <- Sys.Date() %>% with_tz("UTC")
 
-  if (floor(difftime(Sys.Date(), daylight_span$sunset ,tz = arcola_location$tz ,units = "days")) != 0) {
-    daylight_span <- sunrise.set(arcola_location$lat, arcola_location$lng, date = Sys.Date() %>% ymd(.,tz = arcola_location$tz)) #sunrise.set gives info for yesterday, so we increment 1 day
+  if (floor(abs(difftime(right_now_UTC, daylight_span$sunrise ,tz = arcola_location$tz ,units = "days"))) != 0) {
+    daylight_span <- sunrise.set(arcola_location$lat, arcola_location$lng, date = ymd(right_now_UTC, tz = "UTC")) #sunrise.set gives info for yesterday, so we increment 1 day
   }
 
-  this_day <- interval(daylight_span$sunrise, daylight_span$sunset, tzone = arcola_location$tz)
-  right_now <- ymd_hms(Sys.time(), tz = "US/Eastern")
+  this_day <- interval(daylight_span$sunrise, daylight_span$sunset, tzone = "arcola_location$tz"UTC)
+  right_now <- ymd_hms(Sys.time(), tz = "UTC")
   
   while(right_now %within% this_day) {
     xml1 <- get_xml_block(url_a, "eM200", "PW")
