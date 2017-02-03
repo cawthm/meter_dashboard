332840e20f1f2cb6f975e506d9c6058531e14945
cawthm <cawthm@mac.com>
2017-02-03 14:22:45 -0600

    updated shell script to run all files

--- run_all_meter_scripts.sh
+++ run_all_meter_scripts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-Rscript laurita_alpha.R &
-Rscript johnston_alpha.R &
-Rscript aina_all.R
-Rscript arcola_meter.R
+Rscript aina1_meter.R &
+Rscript aina2_meter.R &
+Rscript arcola_meter.R &
+Rscript laurita_meter.R &
+Rscript laurita_obvius.R &
+johnston_growatt_obvius.R &
+johnston_siel_obvius.R &
