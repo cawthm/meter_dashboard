8796d88b3120d5ff2b7dca9747ba1e0034597d2d
cawthm <cawthm@mac.com>
2017-02-04 14:31:41 -0600

    changed tibble() to data_frame(); was giving issue on vm

--- helper_funcs.R
+++ helper_funcs.R
@@ -35,11 +35,11 @@ meter_info_snapshot <- function(my_url, login, password, tz, meter_type) {
   }  
   if (meter_type == "obvius") {
     my_xml1 <- get_xml_block(my_url, login, password)
-    df <- tibble(field = my_xml1 %>% xml_find_all(".//point") %>% xml_attr("name"),
+    df <- data_frame(field = my_xml1 %>% xml_find_all(".//point") %>% xml_attr("name"),
                  value = my_xml1 %>% xml_find_all(".//point") %>% xml_attr("value") %>% as.numeric()
     )
     
-    date_time_utc <- xml2::xml_find_first(my_xml1, ".//time") %>% xml_contents() %>% lubridate::ymd_hms(tz = tz) %>% with_tz("UTC")
+    date_time_utc <- xml2::xml_find_first(my_xml1, ".//time") %>% xml_contents() %>% with_tz("UTC")
     inst_kw <- df %>% filter(field == "Power Instantaneous, total all phases") %>% .$value
     cum_kwh <- df %>% filter(field == "Energy Net") %>% .$value 
   }
