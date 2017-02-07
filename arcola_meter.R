85084f8656c809b4c03987b8193d7cdba0f89e07
cawthm <cawthm@mac.com>
2017-02-07 11:56:20 -0600

    corrected loop which checks current time vs today's date (it was never exiting loop)

--- arcola_meter.R
+++ arcola_meter.R
@@ -17,19 +17,22 @@ while(year(Sys.Date()) <= 2017) { # everything in UTC
   daylight_span <- sunrise.set(location$lat, location$lng, date = ymd("2016-01-01", tz = "UTC")) #
   
   if (abs(difftime(Sys.time() %>% with_tz("UTC"), daylight_span$sunset ,tz = location$tz ,units = "hours")) >= 24) {
-    daylight_span <- sunrise.set(location$lat, location$lng, date = Sys.Date() %>% ymd(.,tz = "UTC")) #
+    daylight_span <- sunrise.set(location$lat, location$lng, date = Sys.Date() %>% with_tz("UTC")) #
     source("helper_funcs.R") # refresh once per day
   }
   
-  this_day <- interval(daylight_span$sunrise, daylight_span$sunset, tzone = "UTC")
+  this_day <- interval(daylight_span$sunrise, daylight_span$sunset)
   right_now <- Sys.time() %>% with_tz("UTC")
   
   while(right_now %within% this_day) {
     try(
       meter_info_snapshot(.creds$url, login = .creds$login, password = .creds$pass, tz = location$tz, meter_type = .creds$type) %>% 
-        write_csv(.,"data/arcola_meter.csv", append = TRUE)
+        write_csv(.,"data/arcola_meter.csv", append = TRUE),
+      silent = TRUE
     )
     Sys.sleep(15)
+    
+    right_now <- Sys.time() %>% with_tz("UTC")
   }
   
 }
