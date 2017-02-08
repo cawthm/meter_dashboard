9e7f17e4e8e6d167c2c23d264a172f9b47a94648
cawthm <cawthm@mac.com>
2017-02-08 09:20:40 -0600

    updated all individual meters to correctly check if it's still dayday within while loop

--- laurita_meter.R
+++ laurita_meter.R
@@ -16,19 +16,24 @@ while(year(Sys.Date()) <= 2017) { # everything in UTC
   daylight_span <- sunrise.set(location$lat, location$lng, date = ymd("2016-01-01", tz = "UTC")) #
   
   if (abs(difftime(Sys.time() %>% with_tz("UTC"), daylight_span$sunset ,tz = location$tz ,units = "hours")) >= 24) {
-    daylight_span <- sunrise.set(location$lat, location$lng, date = Sys.Date() %>% ymd(.,tz = "UTC")) #
+    daylight_span <- sunrise.set(location$lat, location$lng, date = as.character((Sys.Date() %>% with_tz("UTC")))) #
     source("helper_funcs.R") # refresh once per day
   }
   
-  this_day <- interval(daylight_span$sunrise, daylight_span$sunset, tzone = "UTC")
+  this_day <- interval(daylight_span$sunrise, daylight_span$sunset)
   right_now <- Sys.time() %>% with_tz("UTC")
   
-  while( right_now %within% this_day ) {
+  while(right_now %within% this_day) {
     try(
-      meter_info_snapshot(.creds$url, login = .creds$login, password = .creds$pass, tz = location$tz, meter_type = .creds$type) %>% 
-        write_csv(.,"data/laurita_meter.csv", append = TRUE)
+      x <- meter_info_snapshot(.creds$url, login = .creds$login, password = .creds$pass, tz = location$tz, meter_type = .creds$type),
+      silent = TRUE
+      
     )
+    
+    write_csv(x, paste0("data/",location$creds_name,".csv"), append = TRUE)
+    
     Sys.sleep(15)
+    
+    right_now <- Sys.time() %>% with_tz("UTC")
   }
   
-}
